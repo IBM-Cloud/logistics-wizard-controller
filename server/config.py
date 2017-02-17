64c63d113dc8e99883d822a3d6305e568e679473
Frederic Lavigne <frederic.lavigne@fr.ibm.com>
2017-02-17 13:16:56 +0100

    set recommendation package name to a shorter name https://github.com/IBM-Bluemix/logistics-wizard-recommendation/issues/20

--- server/config.py
+++ server/config.py
@@ -8,4 +8,4 @@ class Config(object):
 
     OPENWHISK_URL = env.get('OPENWHISK_URL', 'https://openwhisk.ng.bluemix.net')
     OPENWHISK_AUTH = env.get('OPENWHISK_AUTH')
-    OPENWHISK_PACKAGE = env.get('OPENWHISK_PACKAGE', 'logistics-wizard-recommendation')
+    OPENWHISK_PACKAGE = env.get('OPENWHISK_PACKAGE', 'lwr')
